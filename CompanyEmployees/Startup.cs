c638e7bce9b78b82731a6a3e6dc0da8986cfaa18
zawette <zawette@gmail.com>
2020-10-12 13:55:38 +0100

    xml formatter

--- CompanyEmployees/Startup.cs
+++ CompanyEmployees/Startup.cs
@@ -28,7 +28,7 @@ namespace CompanyEmployees
         {
             services.ConfigureCors();
             services.ConfigureSqlContext(Configuration);
-            services.AddControllers();
+            services.AddControllers(config => config.ReturnHttpNotAcceptable = true).AddXmlDataContractSerializerFormatters();
             services.AddScoped<ILoggerManager, LoggerManager>();
             services.AddScoped<IRepositoryManager, RepositoryManager>();
             services.AddAutoMapper(typeof(Startup));
