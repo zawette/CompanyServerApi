bf2056c8d1b2262e9dc1cf9c2829c07cf7ccf8d4
zawette <zawette@gmail.com>
2020-10-14 14:15:14 +0100

    put Requests

--- CompanyEmployees/Profiles/MappingProfile.cs
+++ CompanyEmployees/Profiles/MappingProfile.cs
@@ -14,6 +14,8 @@ namespace CompanyEmployees.Profiles
             CreateMap<Employee, EmployeeDto>();
             CreateMap<CompanyForCreationDto, Company>();
             CreateMap<EmployeeForCreationDto, Employee>();
+            CreateMap<EmployeeForUpdateDto, Employee>();
+            CreateMap<CompanyForUpdateDto, Company>();
         }
     }
 }
---
+++ Entities/Dtos/EmployeeForUpdateDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Entities.Dtos
+{
+    public class EmployeeForUpdateDto
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Position { get; set; }
+    }
+}
