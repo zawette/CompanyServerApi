fa145651c65818bae1331b4a05d4a70f2590b8ed
zawette <zawette@gmail.com>
2020-10-20 13:47:28 +0100

    Search

--- Entities/RequestFeatures/EmployeeParameters.cs
+++ Entities/RequestFeatures/EmployeeParameters.cs
@@ -1,14 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Entities.RequestFeatures
+﻿namespace Entities.RequestFeatures
 {
     public class EmployeeParameters : RequestParameters
     {
         public uint MinAge { get; set; }
         public uint MaxAge { get; set; } = int.MaxValue;
         public bool ValidAgeRange => MaxAge > MinAge;
-
+        public string SearchTerm { get; set; }
     }
 }
